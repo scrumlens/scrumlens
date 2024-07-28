f88eb2f5296988a5d54bcb2e868cf471b9178d4d
Anton Reshetov <reshetov.art@gmail.com>
2024-07-28 10:04:51 +0300

    feat(server): add auth api (#3)

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,13 +1,36 @@
 import { styleText } from 'node:util'
 import { Elysia, t } from 'elysia'
+import { swagger } from '@elysiajs/swagger'
+import mongoose from 'mongoose'
+import { version } from '../package.json'
 import ws from './routes/ws'
+import auth from './routes/auth'
+
+mongoose.connect(Bun.env.MONGO_URL)
 
 const app = new Elysia()
 
-app.use(ws).listen(Bun.env.API_PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(
-    styleText('green', `\nServer is running on`),
-    styleText('cyan', `http://localhost:${Bun.env.API_PORT}\n`),
+app
+  .use(
+    swagger({
+      documentation: {
+        info: {
+          title: 'Scrumlens API',
+          version,
+        },
+        tags: [{ name: 'Auth', description: 'Authentication' }],
+      },
+    }),
   )
-})
+  .use(auth)
+  .use(ws)
+  .listen(Bun.env.API_PORT, () => {
+    // eslint-disable-next-line no-console
+    console.log(
+      styleText('green', `\nServer is running\n`),
+      styleText('green', 'API: '),
+      styleText('cyan', `http://localhost:${Bun.env.API_PORT}\n`),
+      styleText('green', 'Docs:'),
+      styleText('cyan', `http://localhost:${Bun.env.API_PORT}/swagger\n`),
+    )
+  })
