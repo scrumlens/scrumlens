6cdafc0f8b41c87cd0c34cdc61670865ceaecaee
Anton Reshetov <reshetov.art@gmail.com>
2024-07-30 11:00:39 +0300

    feat: add reconect websocket

--- apps/client/components/board/composables/useWebsocket.ts
+++ apps/client/components/board/composables/useWebsocket.ts
@@ -1,18 +1,66 @@
-let ws: WebSocket
+/* eslint-disable no-console */
+import type { WebSocketEventData } from '../../../../../shared/types'
+import { useBoard } from './useBoard'
+import type { BoardResponse } from '~/services/api/generated'
 
-export function useWebsocket(id: string) {
+const isDev = import.meta.env.MODE === 'development'
+
+let ws: WebSocket
+const MAX_RECONNECT_ATTEMPTS = 5
+const RECONNECT_DELAY = 3000
+
+const wsUrl = ref('')
+const reconnectAttempts = ref(0)
+
+const { boardRaw } = useBoard()
+
+function reconnect() {
+  reconnectAttempts.value++
+
+  console.log('[WebSocket]: reconnect...')
+
+  if (reconnectAttempts.value > MAX_RECONNECT_ATTEMPTS) {
+    console.log('[WebSocket]: max reconnect attempts')
+    return
+  }
+
+  setTimeout(() => {
+    connect()
+  }, RECONNECT_DELAY)
+}
+
+function connect() {
+  ws = new WebSocket(wsUrl.value)
+
+  ws.onmessage = (event) => {
+    const message = JSON.parse(event.data) as WebSocketEventData<BoardResponse>
+    if (message.type === 'board:update') {
+      boardRaw.value = message.data
+    }
+  }
+
+  ws.onopen = () => {
+    console.log('[WebSocket]: opened')
+    reconnectAttempts.value = 0
+  }
+
+  ws.onclose = () => {
+    console.log('[Websocket]: closed')
+    reconnect()
+  }
+}
+
+export function useWebSocket(url: string) {
   const {
     public: { websocketUrl },
   } = useRuntimeConfig()
 
   if (ws) {
-    return ws
+    ws.close()
   }
 
-  const isDev = import.meta.env.MODE === 'development'
-  const wsUrl = isDev ? `${websocketUrl}/${id}` : `/ws/${id}`
-
-  ws = new WebSocket(wsUrl)
+  wsUrl.value = isDev ? `${websocketUrl}/${url}` : `/ws/${url}`
+  connect()
 
   return ws
 }
