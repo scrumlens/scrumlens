b04d0b4c0cdfedfe595c1d09b42f829e413644a1
Anton Reshetov <reshetov.art@gmail.com>
2024-08-13 08:15:23 +0300

    feat: add shadcn components

--- apps/client/components/ui/shadcn/avatar/index.ts
+++ apps/client/components/ui/shadcn/avatar/index.ts
@@ -9,6 +9,7 @@ export const avatarVariant = cva(
   {
     variants: {
       size: {
+        xs: 'h-8 w-8 text-xs',
         sm: 'h-10 w-10 text-xs',
         base: 'h-16 w-16 text-2xl',
         lg: 'h-32 w-32 text-5xl',
