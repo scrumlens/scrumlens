038f5e6aa01019b5ce01d747828e734730b466ba
Anton Reshetov <reshetov.art@gmail.com>
2024-08-29 16:35:36 +0300

    polish(client): same style for filter & sorting

--- apps/client/components/header/tools/Sorting.vue
+++ apps/client/components/header/tools/Sorting.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ArrowUpDown, Dot } from 'lucide-vue-next'
+import { ArrowUpDown } from 'lucide-vue-next'
 import { useSort } from '@/components/board/composables'
 
 const { selected, options } = useSort()
@@ -15,22 +15,26 @@ const { selected, options } = useSort()
       >
         <div
           v-if="selected !== 'custom'"
-          class="w-1.5 h-1.5 rounded-full bg-red-500 absolute top-1 right-1"
+          class="w-1.5 h-1.5 rounded-full bg-blue-500 absolute top-1 right-1"
         />
         <ArrowUpDown class="w-3.5 h-3.5" />
       </Button>
     </DropdownMenuTrigger>
     <DropdownMenuContent>
-      <DropdownMenuItem
-        v-for="i in options"
-        :key="i.value"
-        as-child
-        @click="selected = i.value"
-      >
-        <div :class="{ 'bg-slate-100 dark:bg-slate-800': selected === i.value }">
-          {{ i.label }}
+      <DropdownMenuLabel class="flex items-center justify-between gap-2 h-10">
+        <div>
+          Sort by
         </div>
-      </DropdownMenuItem>
+      </DropdownMenuLabel>
+      <DropdownMenuRadioGroup v-model="selected">
+        <DropdownMenuRadioItem
+          v-for="i in options"
+          :key="i.value"
+          :value="i.value"
+        >
+          {{ i.label }}
+        </DropdownMenuRadioItem>
+      </DropdownMenuRadioGroup>
     </DropdownMenuContent>
   </DropdownMenu>
 </template>
